18445ebbdc22aa7b6b88a8ffdcd299d852ff6840
chetandivekar <chetandivekar6@gmail.com>
2022-12-26 20:39:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let date = new Date;
+let hour  = date.getHours();
+let mainHour = hour - 12;
+let min = date.getMinutes();
+let mainSec = date.getSeconds();
+
+
+let a = document.getElementById("hour");
+let b = document.getElementById("minute");
+let c = document.getElementById("second");
+
+
+setInterval(()=>{
+    let date = new Date;
+let hour  = date.getHours();
+let mainHour = hour - 12;
+let min = date.getMinutes();
+let mainSec = date.getSeconds();
+
+if(mainHour<10){
+    a.innerHTML = "0" + mainHour;
+
+}
+else{
+    a.innerHTML = mainHour;
+}
+if(min<10){
+    b.innerHTML = "0" +min;
+}
+else{
+    b.innerHTML = min;
+}
+if(mainSec<10){
+    c.innerHTML = "0" +mainSec ;
+}
+else{
+    c.innerHTML = mainSec ;
+}
+   
+},1000)
